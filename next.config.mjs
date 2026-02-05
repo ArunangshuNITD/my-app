58c3ece8959db90e359005d936ea2a9848fdef15
ArunangshuNITD <Arunangshud3@gmail.com>
2026-02-05 07:11:57 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: false, // <--- DISABLES DOUBLE RENDERING
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com" },
       { protocol: "https", hostname: "avatars.githubusercontent.com" },
       { protocol: "https", hostname: "api.dicebear.com" },
-      { protocol: "https", hostname: "res.cloudinary.com" }, // <--- ADD THIS LINE
+      { protocol: "https", hostname: "res.cloudinary.com" },
     ],
   },
   experimental: {
